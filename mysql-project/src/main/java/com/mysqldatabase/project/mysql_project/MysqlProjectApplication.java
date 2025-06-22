cf88b973bf98f22eb5578e75049179984f39821e
sumit@808085 <sumitrathod808085@gmail.com>
2025-06-22 17:40:16 +0530

    main body cahnges..

--- mysql-project/src/main/java/com/mysqldatabase/project/mysql_project/MysqlProjectApplication.java
+++ mysql-project/src/main/java/com/mysqldatabase/project/mysql_project/MysqlProjectApplication.java
@@ -5,13 +5,27 @@ public class MysqlProjectApplication
 {
 	public PreparedStatement ps ;
 	public ResultSet rs;
-	public Scanner sc=new Scanner(System.in);
+	public static Scanner sc=new Scanner(System.in);
 	public static MysqlProjectApplication a1;
 	public static void main(String[] args) 
-	{
-			 a1 =new MysqlProjectApplication();
-				//a1.read();
-				a1.write();
+	{   int ch;
+		 a1 =new MysqlProjectApplication();   
+		 do
+		{
+			System.out.println("\n\n1.read\n 2.write\n 3.exit");
+			System.out.println("Enter Your Choice:");
+			 ch =sc.nextInt();
+			switch(ch)
+			{
+				case 1:a1.read();
+				     break;
+				case 2:a1.write();
+				     break;
+				case 3:System.out.println("thanks for using my software");	
+				     break;
+				default:System.out.println("invalid choice..");	  	 
+			}
+		}while(3!=ch);
 	}
 
 	public void read() 
@@ -20,11 +34,13 @@ public class MysqlProjectApplication
 			{
 				 ps = Comman.con().prepareStatement("select * from demo");
 					 rs = ps.executeQuery();
+					 System.out.println("===============================STUDENT-INFO===========================================");
 					while(rs.next()) 
 					{
-						System.out.println("id_is :\t" + rs.getInt(1) + " \t name: \t" + rs.getString(2) + "\t gmail: \t"
+						System.out.println("id_is :  " + rs.getInt(1) + " \t name: " + rs.getString(2) + "\t gmail: "
 								+ rs.getString(3));
 					}
+					System.out.println("=======================================================================================");
 			} catch (Exception e) 
 			{
 				e.printStackTrace();
